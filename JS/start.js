32eb81a0efff744afa7acc92d4ff52f13ad66722
Matthew Wong <matthewwong3.14@gmail.com>
2015-06-19 19:33:07 -0700

    start button test
    removes itself and any <p> tags

--- JS/start.js
+++ JS/start.js
@@ -13,4 +13,12 @@ function test(){
 	$("#test").css("left","500px");
 	$("#test").css("top","80px");
 	$("#test").css("fontSize","300%");
+	
+	var startBtn = $('<input type="button" value="Start Game" id="start">');
+	$("body").append(startBtn);
+	
+	$("#start").click(function() {
+		$("#start").remove();
+		$("p").remove();
+	});
 }
