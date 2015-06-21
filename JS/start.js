6340c49c53e27f9df23ee5661e907accad997b3e
Matthew Wong <matthewwong3.14@gmail.com>
2015-06-21 00:37:06 -0700

    Cleaned up code and commented

--- JS/start.js
+++ JS/start.js
@@ -1,54 +1,53 @@
+//Starts the game
 function startGame(){
-	test();
+	text();
+	buttons();
 }
 
-function test(){
+//creates the text of the start page
+function text(){
+	//Creates the title text
 	var t = document.createElement("span");
 	var node1 = document.createTextNode("Knight Trials");
 	t.appendChild(node1);
-	t.setAttribute("id","test");
+	t.setAttribute("id","title");
 	t.setAttribute("class","getRidOf");
 	document.body.appendChild(t);
 	
-	// var docWidth = $(document).width();
-	// var docCenter = (docWidth/2);
-	// var titleWidth = ($("#test").width());
-	// var half = (titleWidth/2);
-	// var correctNum = (docCenter-half);
- 	
-	// document.write(docWidth + " ");
-	// document.write(docCenter + " ");
-	// document.write(titleWidth + " ");
-	// document.write(half + " ");
-	// document.write(correctNum + " ");
-	
-	$("#test").css('text-align','center');
-	$("#test").css("width","100%");
-	$("#test").css("left","0");
-	
-	 $("#test").css("position","absolute");
-	// $("#test").css("left", (docCenter - ($("#test").width()/2) ) + "px");
-	// $("#test").css("left", correctNum+"px");
-	 $("#test").css("fontSize","300%");
-	
+	//title styling
+	$("#title").css('text-align','center');
+	$("#title").css("width","100%");
+	$("#title").css("left","0");
+	$("#title").css("position","absolute");
+	$("#title").css("fontSize","300%");
+}
+
+//creates the buttons of the start page
+function buttons(){
+	//creates start button
 	var startBtn = $('<input type="button" value="Start Game" id="start">');
 	$("body").append(startBtn);
 	
+	//start button functionality
 	$("#start").click(function() {
 		$("#start").remove();
 		$("p").remove();
 	});
 	
+	//creates delete button
 	var deleteBtn = $('<input type="button" value="Delete text" id="del">');
 	$("body").append(deleteBtn);
 	
+	//delete button functionality
 	$("#del").click(function() {
 		$(".getRidOf").remove();
 	});
 	
+	//creates deleteAll button
 	var deleteAll = $('<input type="button" value="delete all" id="deleteIt">');
 	$("body").append(deleteAll)
 	
+	//deleteAll button functionality
 	$("#deleteIt").click(function()
 	{
 		$(".getRidOf").remove();
@@ -58,5 +57,4 @@ function test(){
 		$("#deleteIt").remove();
 		
 	});
-
 }
